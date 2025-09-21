2ff5653d46f1de244aff7ed8d4a2c315e0556957
Hqb4102k4 <dung98pt@gmail.com>
2025-09-21 22:12:41 +0700

    Route group

--- laravel/learnLaravel/routes/web.php
+++ laravel/learnLaravel/routes/web.php
@@ -9,3 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "Hello World";
 });
+
+Route::prefix('details')->group(function () {
+    Route::get('about', function () {
+        return "This is about page";
+    });
+
+    Route::get('contact', function () {
+        return "This is contact page";
+    });
+});
