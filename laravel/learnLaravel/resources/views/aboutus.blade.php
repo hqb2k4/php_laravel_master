4a05d37a316ec8b15e5018b3be0582c9c2f7e7d3
Hqb4102k4 <dung98pt@gmail.com>
2025-09-21 23:13:25 +0700

    Blade Directives

--- laravel/learnLaravel/resources/views/aboutus.blade.php
+++ laravel/learnLaravel/resources/views/aboutus.blade.php
@@ -3,4 +3,8 @@
     <p>This is the about us page of our Laravel application.</p>
     <h2>Name: {{ $name }}</h2>
     <h2>Age: {{ $age }}</h2>
+
+    @for ($i = 0; $i < 5; $i++)
+        <p>Iteration {{ $i + 1 }}: Breathing in, I calm body and mind. Breathing out, I smile./p>   
+    @endfor
 </div>
