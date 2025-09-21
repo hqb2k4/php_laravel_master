a60e5cfa0e0bea397fa4674979c465156a314461
Hqb4102k4 <dung98pt@gmail.com>
2025-09-21 23:43:47 +0700

    Layouts with Blade for Reusable Templates

--- laravel/learnLaravel/resources/views/aboutus.blade.php
+++ laravel/learnLaravel/resources/views/aboutus.blade.php
@@ -1,4 +1,10 @@
-<div>
+@extends('layout.master')
+
+@section('title', 'About Us')
+
+@section('content')
+    <h1>About Page</h1>
+    <p>Nội dung giới thiệu...</p>
     <h1>About Us</h1>
     <p>This is the about us page of our Laravel application.</p>
     <h2>Name: {{ $name }}</h2>
@@ -6,9 +12,9 @@
 
     <!-- Blade Directive -->
     @for ($i = 0; $i < 5; $i++)
-        <p>Iteration {{ $i + 1 }}: Breathing in, I calm body and mind. Breathing out, I smile./p>   
+        <p>Iteration {{ $i + 1 }}: Breathing in, I calm body and mind. Breathing out, I smile.</p>
     @endfor
 
     <!-- SubView -->
     @include('SubView.Input', ['myName' => $name])
-</div>
+@endsection
---
+++ laravel/learnLaravel/resources/views/layout/master.blade.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>@yield('title')</title>
+  </head>
+  <body>
+    <header>Header của web</header>
+
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <footer>Footer của web</footer>
+  </body>
+</html>
