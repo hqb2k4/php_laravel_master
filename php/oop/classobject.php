aaf84c5231be75d4763cddd93211fa5e0d5868a5
hqb2k4 <quocbao41004@gmail.com>
2025-09-28 15:16:19 +0700

    Create an Object and set property values

--- php/oop/classobject.php
+++ php/oop/classobject.php
@@ -7,5 +7,18 @@ class Car {
     public $color;
     public $model;
 
-
+    //Method
+    public function run() {
+        return "The car is running";
+    }
 }
+
+// Object
+$myCar = new Car();
+
+// Set property values
+$myCar->color = "black";    
+$myCar->model = "Volvo";
+
+echo "My car is a " . $myCar->color . " " . $myCar->model . ".<br>";
+echo $myCar->run();
